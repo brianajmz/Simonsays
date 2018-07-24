e6590a0677365a995f1c88b7442a081c37f96541
brianajmz <bribri4304@icloud.com>
2018-07-24 13:16:59 -0500

    adding practice scripts

---
+++ scripts/get_user_input.py
@@ -0,0 +1,11 @@
+from getpass import getpass
+# this script shows how to get user input
+
+print "What is your name?"
+name = raw_inpuut()
+
+color = getpass ( ' What is you favorite color? ' )
+
+print "Hi", name
+print "Your favorite color is: ", color
+
