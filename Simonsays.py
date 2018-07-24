41d4578098362faed2f7ea1e6fbbc046a05ba72d
brianajmz <bribri4304@icloud.com>
2018-07-24 15:45:30 -0500

    adds random colors to color sequence

--- Simonsays.py
+++ Simonsays.py
@@ -9,23 +9,32 @@ GPIO.setwarnings(False)
 
 # this script bliks one of four random colors on the RGB LED
 colors = ['R', 'G', 'B', 'Y']
+frequencies = [200, 300, 400, 500]
 R_pin = 11
 G_pin = 12
 B_pin = 13
 LED.setup(R_pin, G_pin, B_pin)
 
-
+buzz_pin = 32
+GPIO.setup(buzz_pin,GPIO.OUT)   
+Buzz = GPIO.PWM(buzz_pin,1000)
+color_sequence = []
+frequency_sequence = []
 # this script appends a value to a list 
 def append_list():
     while True:
-        for i in range(0, len(colors)):
-            n = random.randint(0,3)
-            colors.append(colors[n])
-            color_string = ' '.join(colors)
-            LED.setColor(colors[n])
+        n = random.randint(0,3)
+        color_sequence.append(colors[n])
+        frequency_sequence.append(frequencies[n])
+        for i in range(0, len(color_sequence)):
+            Buzz.ChangeFrequency(frequency_sequence[i])
+            Buzz.start(50)
+            LED.setColor(color_sequence[i]) 
             time.sleep(0.5)
+            Buzz.stop()
             LED.noColor()
             time.sleep(0.5)
+            
 
 if __name__ == '__main__':
     try:
